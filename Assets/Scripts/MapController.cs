dc770e3bf7af99630f77792e83eb1911851aefca
Dong Kyu Lee <stvboy00@gmail.com>
2023-01-03 23:27:36 +0900

    created map and character for test

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public Vector3 startPos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
